f9d4e81e3acc1423872c5156b4ff3e5d655fe5c9
Jethro Kuan <jethrokuan95@gmail.com>
2016-02-21 18:51:37 +0800

    Initial Commit

---
+++ tree.fish
@@ -0,0 +1,6 @@
+function t1; tree --dirsfirst -ChFLQ 1 $argv; end
+function t2; tree --dirsfirst -ChFLQ 2 $argv; end
+function t3; tree --dirsfirst -ChFLQ 3 $argv; end
+function t4; tree --dirsfirst -ChFLQ 4 $argv; end
+function t5; tree --dirsfirst -ChFLQ 5 $argv; end
+function t6; tree --dirsfirst -ChFLQ 6 $argv; end
