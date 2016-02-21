b41b2fdd8ffe67208715f862a29a7259c4a656e0
Jethro Kuan <jethrokuan95@gmail.com>
2016-02-21 19:00:27 +0800

    make t1 even simpler

--- functions/tree.fish
+++ functions/tree.fish
@@ -1,4 +1,4 @@
-function t1; tree --dirsfirst -ChFLQ 1 $argv; end
+function t; tree --dirsfirst -ChFLQ 1 $argv; end
 function t2; tree --dirsfirst -ChFLQ 2 $argv; end
 function t3; tree --dirsfirst -ChFLQ 3 $argv; end
 function t4; tree --dirsfirst -ChFLQ 4 $argv; end
